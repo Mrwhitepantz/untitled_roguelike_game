7ccb5e5b19ffde720428e58fc1c809c3d92cc871
Joon-Z <39546939+Joon-Z@users.noreply.github.com>
2023-02-03 10:08:06 -0800

    OO-ified movement script
    Reimplemented movement to be more object oriented.
    - Added FixedUpdate() - deals with anything that is manipulating rigidbodies
    - Added more instance variables
    - Movement speed can now be changed in the script

--- pokemon gone mad/Assets/Scripts/TopDownController.cs
+++ pokemon gone mad/Assets/Scripts/TopDownController.cs
@@ -4,33 +4,65 @@ using UnityEngine;
 
 public class TopDownController : MonoBehaviour
 {
-    public Rigidbody2D body;
     public SpriteRenderer spriteRenderer;
     public Animator animator;
+    public Rigidbody2D body;
 
-    public float moveSpeed = 4f;
-    private Vector2 direction;
-
+    //Variables related to movement
+    public float moveSpeed;
+    public float maxSpeed;
+    public float linearDrag;
+    //private Vector2 direction;
+    private float X;
+    private float Y;
 
     // Start is called before the first frame update
+    // Initialize starting values here (ex. character speed, character direction)
     void Start()
     {
-        
+        moveSpeed = 5f;
+        linearDrag = 10f;
     }
 
     // Update is called once per frame
+    // Code that affects getting input values
     void Update()
     {
-
-        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
-        body.velocity = direction * moveSpeed;
-        if(direction.x != 0 || direction.y != 0)
+        X = getInput().x;
+        Y = getInput().y;
+        //Jame's code
+        //direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized; //responsible for player movement
+        //body.velocity = direction * moveSpeed;
+        /*if(direction.x != 0 || direction.y != 0)
         {
             animator.SetFloat("speed", 1);
         }
         else
         {
             animator.SetFloat("speed", 0);
+        }*/
+    }
+
+    // Code that affects rigid body physics
+    void FixedUpdate()
+    {
+        //body.MovePosition(body.position + direction * moveSpeed * Time.fixedDeltaTime);
+        movePlayer();
+        if (Mathf.Abs(body.velocity.x) > maxSpeed)
+        {
+            
         }
+    }
+
+    // Moves the player
+    private void movePlayer()
+    {
+        body.velocity = new Vector2(X * moveSpeed, Y * moveSpeed);
+    }
+
+    // Get's x and y values
+    private Vector2 getInput()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 }
