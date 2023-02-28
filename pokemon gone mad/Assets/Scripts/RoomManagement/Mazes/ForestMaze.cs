b0d04b46c04655c6a31e02e242858943574ba2bc
James Fisher <mrwhitepantz@hotmail.com>
2023-02-27 16:41:49 -0800

    Update ForestMaze.cs

--- pokemon gone mad/Assets/Scripts/RoomManagement/Mazes/ForestMaze.cs
+++ pokemon gone mad/Assets/Scripts/RoomManagement/Mazes/ForestMaze.cs
@@ -4,31 +4,17 @@ using UnityEngine;
 
 public class ForestMaze : MazeGenerator
 {
-    private int gridWidth;
-    private int gridHeight;
-    private RoomBuilder.GridSpaceType[,] grid;
-    private struct Leaf
-    {
-        int minimumSize;
-
-    }
-    public void NewMaze(int width, int height, RoomBuilder.GridSpaceType[,] gridArray)
-    {
-        gridWidth = width;
-        gridHeight = height;
-        grid = gridArray;
-    }
-    private void CreateFloors(RoomBuilder.GridSpaceType[,] grid)
-    {
-        CreateRooms(grid);
-    }
-
-    private void CreateWalls(int roomWidth, int roomHeight, RoomBuilder.GridSpaceType[,] gridArray)
+    public void CreateFloors(int width, int height, RoomBuilder.GridSpaceType[,] gridArray)
     {
 
     }
 
-    private void CreateRooms(RoomBuilder.GridSpaceType[,] grid)
+    public void CreateWalls(int roomWidth, int roomHeight, RoomBuilder.GridSpaceType[,] gridArray)
+    {
+
+    }
+
+    private void CreateClearing(RoomBuilder.GridSpaceType[,] grid)
     {
         
     }
